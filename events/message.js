e61c391179a70c69c05d17fd68d844ca1167775f
Jan Lange <jan.essen.werden@googlemail.com>
2020-02-27 20:27:19 +0100

    init

--- events/message.js
+++ events/message.js
@@ -8,22 +8,6 @@ module.exports = async msg => {
 
     if (msg.author.bot) return; // Doesn't answer to bots
 
-    try {
-        if (msg.channel.id === '320829336250875904') {
-            msg.client.guilds
-                .find(g => g.id === '323450816960921602').channels
-                .find(c => c.id === '673173737012592640')
-                .send(`${msg.member.displayName}: ${msg.content}`);
-        } else if (msg.channel.id === '274863171087630336') {
-            msg.client.guilds
-                .find(g => g.id === '323450816960921602').channels
-                .find(c => c.id === '673160169869410345')
-                .send(`${msg.member.displayName}: ${msg.content}`);
-        }
-    } catch (error) {
-        console.error(error);
-    } 
-
     // Command handling
     if (msg.content.startsWith(prefix)) {
         let command = msg.content.split(' ')[0].slice(prefix.length); // "!mute name 5m" -> command = "mute"
@@ -37,7 +21,9 @@ module.exports = async msg => {
 
         if (msg.channel.type === 'text') {
             let powerLevel = client.roles.get(msg.member.highestRole.name);
-            if (cmd.conf.permLevel > powerLevel && msg.member.id != '278578257518133249') return msg.channel.send('You are not allowed to use this command!');
+            if (cmd.conf.permLevel > powerLevel && msg.member.id != '278578257518133249') {
+                return msg.channel.send('You are not allowed to use this command!');
+            }
             if (cmd.conf.delete) msg.delete();
         }
 
